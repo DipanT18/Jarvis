c3f76221572cecaf96d1ae1de12f961638ef7ae5
Dipan Timalsina <dipantimalsina055@gmail.com>
2025-10-30 12:24:03 +0545

    Modified in library

--- Functions/user_input.py
+++ Functions/user_input.py
@@ -3,7 +3,7 @@ from random import choice
 from datetime import datetime
 from Functions.utils import opening_text
 from Functions.Speak import speak
-
+import random
 def take_user_input():
     """Takes user input, recognizes it using Speech Recognition module and converts it into text"""
 
@@ -27,8 +27,9 @@ def take_user_input():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print('Listening....')
-        r.pause_threshold = 1
-        r.adjust_for_ambient_noise(source, duration=1)
+        r.pause_threshold = 0.8
+        r.energy_threshold = 400  
+        r.adjust_for_ambient_noise(source, duration=5)
         audio = r.listen(source)
 
     try:
@@ -36,15 +37,17 @@ def take_user_input():
         query = r.recognize_google(audio, language='en-in')
         print(f"User said: {query}")
         
-        if not ('exit' in query.lower() or 'stop' in query.lower()):
-            speak(choice(opening_text))
-        else:
+        if ('exit' in query.lower()) or( 'stop' in query.lower()):
             hour = datetime.now().hour
             if hour >= 21 or hour < 6:
                 speak("Good night sir, take care!")
             else:
                 speak('Have a good day sir!')
             exit()
+
+        print("i am here")
+        speak(random.choice(opening_text))
+           
     except sr.UnknownValueError:
         speak('Sorry, I could not understand. Could you please say that again?')
         query = 'None'
@@ -56,5 +59,6 @@ def take_user_input():
         speak('Sorry, something went wrong. Could you please try again?')
         print(f"Unexpected error: {e}")
         query = 'None'
+
     
     return query
